d0d869b39871fccd457c54e5f7d1496c927d2e31
Erwin Schens <erwinschens@uni-koblenz.de>
2015-08-03 10:32:54 +0200

    move stream logic to seperate file

--- lib/rstreamor.rb
+++ lib/rstreamor.rb
@@ -2,30 +2,7 @@ require 'rstreamor/version'
 require 'rstreamor/file'
 require 'rstreamor/request'
 require 'rstreamor/response'
+require 'rstreamor/stream'
 module Rstreamor
-
-  def stream(file)
-    request_builder = Rstreamor::Request.new(request, file)
-    response_builder = Rstreamor::Response.new(request_builder)
-    set_response_header(request_builder, response_builder)
-    stream_file(request_builder, response_builder)
-  end
-
-  private
-
-  def stream_file(request_builder, response_builder)
-    content = request_builder.slice_file
-    send_data content, type: request_builder.file_content_type, disposition: 'inline', status: response_builder.response_code
-  end
-
-  def set_response_header(request_builder, response_builder)
-    response.headers['Content-Type'] = request_builder.file_content_type
-    response.headers['Content-Length'] = response_builder.content_length
-    if request_builder.range_header?
-      response.headers['Accept-Ranges'] = 'bytes'
-      response.headers['Cache-Control'] = 'no-cache'
-      response.headers['Content-Range'] = response_builder.content_range
-    end
-  end
-
+  include Rstreamor::Stream
 end
